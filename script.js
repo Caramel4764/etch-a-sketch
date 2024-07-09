0c7476fe249d49c13c56d379b22a521c6aa2a125
Caramel4764 <nina.dong4764@gmail.com>
2024-07-09 00:55:53 -0700

    clicking div changes color to black and prevent dragging the div

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const container = document.querySelector('#container');
 let width = 20;
+let mouseDown = false;
+container.addEventListener('mousedown', ()=>{
+  mouseDown = true;
+})
+container.addEventListener('mouseup', ()=>{
+  mouseDown = false;
+})
 for (let i = 0; i < width; i++) {
   for (let i = 0; i < width; i++) {
     let newPixel = document.createElement("div");
     newPixel.style.width = 600/width + 'px' ;
     newPixel.style.height = 600/width + 'px';
     newPixel.classList.add("pixel");
+    newPixel.addEventListener('mouseover', ()=> {
+      if (mouseDown) {
+      newPixel.style.backgroundColor = 'black';
+      }
+    })
     container.appendChild(newPixel);
   }
 }
