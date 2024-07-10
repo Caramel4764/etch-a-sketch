0b74b00d188ae1a7954579d93e43d52dc7080030
Caramel4764 <nina.dong4764@gmail.com>
2024-07-09 17:54:24 -0700

    feat: add option to change canvas size in beginning or after clicking the adjust page size button

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const container = document.querySelector('#container');
-let width = 20;
 let mouseDown = false;
 container.addEventListener('mousedown', ()=>{
   mouseDown = true;
@@ -10,20 +9,33 @@ container.addEventListener('mouseup', ()=>{
 container.addEventListener('mouseleave', ()=>{
   mouseDown = false;
 })
-for (let i = 0; i < width; i++) {
+function newCanvas () {
+  width = prompt("Please enter the size of your canvas (100 is max)");
+  drawBoard(width);
+}
+const newCanvasBtn = document.querySelector('#page-size-div');
+newCanvasBtn.addEventListener('click', ()=>{
+  newCanvas();
+})
+const drawBoard = function (width) {
+  container.textContent='';
   for (let i = 0; i < width; i++) {
-    let newPixel = document.createElement("div");
-    newPixel.style.width = 600/width + 'px' ;
-    newPixel.style.height = 600/width + 'px';
-    newPixel.classList.add("pixel");
-    newPixel.addEventListener('mousemove', ()=> {
-      if (mouseDown) {
+    for (let i = 0; i < width; i++) {
+      let newPixel = document.createElement("div");
+      newPixel.style.width = 600/width + 'px' ;
+      newPixel.style.height = 600/width + 'px';
+      newPixel.classList.add("pixel");
+      newPixel.addEventListener('mousemove', ()=> {
+        if (mouseDown) {
+          newPixel.style.backgroundColor = 'black';
+        }
+      })
+      newPixel.addEventListener('mousedown', ()=> {
         newPixel.style.backgroundColor = 'black';
-      }
-    })
-    newPixel.addEventListener('mousedown', ()=> {
-      newPixel.style.backgroundColor = 'black';
-    })
-    container.appendChild(newPixel);
+      })
+      container.appendChild(newPixel);
+    }
   }
 }
+let width = prompt("Please enter the size of your canvas (100 is max)");
+drawBoard(width);
