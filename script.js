b4fe3bb14714cc5c0065089caeb24703c1ee7751
popcorn4762 <zoedong2009@gmail.com>
2024-09-02 22:29:10 -0700

    feat: diff color with each stroke and board gets progressively darker.

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const container = document.querySelector('#container');
+const opacityMask = document.querySelector('#opacity-mask')
 let mouseDown = false;
+opacityMask.style.opacity = 0;
+let penColor = 'black';
+function getRandomPenColor() {
+  let ranRed = Math.floor(Math.random()*256);
+  let ranGreen = Math.floor(Math.random()*256);
+  let ranBlue = Math.floor(Math.random()*256);
+  penColor = `rgb(${ranRed}, ${ranGreen}, ${ranBlue})`;
+}
+getRandomPenColor()
 container.addEventListener('mousedown', ()=>{
   mouseDown = true;
 })
 container.addEventListener('mouseup', ()=>{
   mouseDown = false;
+  getRandomPenColor()
+  if (parseFloat(opacityMask.style.opacity) <= 0.9) {
+    opacityMask.style.opacity = parseFloat(opacityMask.style.opacity)+0.1;
+    console.log(opacityMask.style.opacity)
+  }
 })
 container.addEventListener('mouseleave', ()=>{
   mouseDown = false;
@@ -22,16 +37,18 @@ const drawBoard = function (width) {
   for (let i = 0; i < width; i++) {
     for (let i = 0; i < width; i++) {
       let newPixel = document.createElement("div");
-      newPixel.style.width = 600/width + 'px' ;
+      newPixel.style.width = 600/width + 'px';
+      newPixel.style.zIndex = 2;
       newPixel.style.height = 600/width + 'px';
       newPixel.classList.add("pixel");
       newPixel.addEventListener('mousemove', ()=> {
         if (mouseDown) {
-          newPixel.style.backgroundColor = 'black';
+          newPixel.style.backgroundColor = penColor;
         }
       })
+
       newPixel.addEventListener('mousedown', ()=> {
-        newPixel.style.backgroundColor = 'black';
+        newPixel.style.backgroundColor = penColor;
       })
       container.appendChild(newPixel);
     }
